057b28658df7f21c25ab0353ee073760a3820dc3
heitor <heitorluizscdem@gmail.com>
2026-03-10 16:54:22 -0300

    primeiro commit pra preparar o ambiente

---
+++ db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Jjjb3509',
+    database: 'inventario'
+});
+
+connection.connect((err) => {
+    if (err) {
+        return console.error('Erro ao conectar com o banco de dados:' + err.message);
+    }
+    console.log('conectado ao banco de dados!!!');
+});
+
+module.exports = mysql.Connection;
